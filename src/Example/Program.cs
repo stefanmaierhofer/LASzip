e53218d928f65c0c4b741708d14bccadc04cba2a
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-09-25 20:40:07 +0200

    sync

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -16,6 +16,7 @@
 using Aardvark.Base;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -26,7 +27,7 @@ namespace Example
 {
     class Program
     {
-        private static void Test()
+        private static void Test(string dirname)
         {
             var metaFileName = Path.GetFullPath(@"meta.json");
             var meta = JArray
@@ -41,14 +42,52 @@ namespace Example
             var area = meta.Sum(x => x.bounds.Size.X * x.bounds.Size.Y) / 1000000.0;
             WriteLine($"area: {area:N3} km²");
 
-            var bbTotal = new Box3d(meta.Select(x => x.bounds));
-            WriteLine($"bounds: {bbTotal}");
+            var bounds = new Box3d(meta.Select(x => x.bounds));
+            WriteLine($"bounds: {bounds}");
+
+            const double TILE_SIZE = 256.0;
+            var OFFSET = new V3d(0, 0, 50);
+
+            bounds = bounds + OFFSET;
+            if (bounds.Size.Z > TILE_SIZE) throw new InvalidOperationException();
+
+            var bb = new Box3d((bounds.Min / 256).Floor * 256, (bounds.Max / 256).Ceiling * 256);
+            WriteLine($"bounds: {bb}");
+
+            var minIncl = new V2l(bb.Min.X / TILE_SIZE, bb.Min.Y / TILE_SIZE);
+            var maxExcl = new V2l(bb.Max.X / TILE_SIZE + 1, bb.Max.Y / TILE_SIZE + 1);
+            WriteLine($"a: {new Cell(minIncl.X, minIncl.Y, 0, 8).BoundingBox}");
+            WriteLine($"b: {new Cell(maxExcl.X, maxExcl.Y, 0, 8).BoundingBox}");
+
+            var maxCount = 0L;
+            var i = 0;
+            for (var cx = 13074 /*minIncl.X*/; cx < maxExcl.X; cx++)
+            {
+                for (var cy = 22057 /*minIncl.Y*/; cy < maxExcl.Y; cy++)
+                {
+                    var localCell = new Cell(cx, cy, 0, 8);
+                    var localBounds = localCell.BoundingBox - OFFSET;
+                    var candidates = meta.Where(x => x.bounds.Intersects(localBounds)).ToArray();
+                    if (candidates.Length > 0)
+                    {
+                        var sw = new Stopwatch(); sw.Start();
+                        var localCount = candidates.Sum(x => x.count);
+                        if (localCount > maxCount) maxCount = localCount;
+                        //var ps = candidates
+                        //    .Select(x => LASZip.Parser.ReadPoints(Path.Combine(dirname, x.filename), 1024 * 1024))
+                        //    .Select(x => x.SelectMany(chunk => chunk.Positions.Where(p => localBounds.Contains(p)).ToArray()).ToArray())
+                        //    .ToArray()
+                        //    ;
+                        WriteLine($"[{i,6:N0}] {localCell,6} -> {candidates.Length,6:N0} -> {localCount,15:N0}   ({sw.Elapsed})");
+                        i++;
+                    }
+                }
+            }
+            WriteLine(i);
         }
 
         static void Main(string[] args)
         {
-            Test(); return;
-
             var dirname = ".";
             if (args.Length == 1)
             {
@@ -61,6 +100,8 @@ namespace Example
 
             dirname = Path.GetFullPath(dirname);
             WriteLine($"{dirname}:");
+
+            Test(dirname); return;
 
             var totalPointCount = 0L;
             var bounds = Box3d.Invalid;
